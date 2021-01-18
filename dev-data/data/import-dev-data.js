b35b4d20926f489baa690b5a2d38f31bc5a8bbab
sveta <svetlana.shurla4acova@gmail.com>
2021-01-18 20:43:03 +0300

    import test tours in db

---
+++ dev-data/data/import-dev-data.js
@@ -0,0 +1,43 @@
+const dotenv = require('dotenv');
+dotenv.config({ path: './config.env' });
+const fs = require('fs');
+const mongoose = require('mongoose');
+const DB = process.env.DB_URL.replace('<password>', process.env.DB_PASSWORD);
+const Tour = require('../../models/tourModel');
+
+mongoose
+  // .connect(process.env.DB_LOCAL_URL, {
+  .connect(DB, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+  })
+  .then(() => {
+    console.log('DB connection successful!');
+  });
+
+const tours = JSON.parse(fs.readFileSync(`${__dirname}/tours-simple.json`, 'utf-8'));
+
+const importData = async () => {
+  try {
+    await Tour.create(tours);
+  } catch (err) {
+    console.log(err);
+  }
+  process.exit();
+};
+
+const deleteData = async () => {
+  try {
+    await Tour.deleteMany();
+  } catch (err) {
+    console.log(err);
+  }
+  process.exit();
+};
+
+if (process.argv[2] === '--import') {
+  importData();
+} else if (process.argv[2] === '--delete') {
+  deleteData();
+}
