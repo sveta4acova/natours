c050644f50afb4831911e76717584934c669e150
sveta <svetlana.shurla4acova@gmail.com>
2021-03-26 21:51:07 +0300

    import users and reviews from file

--- dev-data/data/import-dev-data.js
+++ dev-data/data/import-dev-data.js
@@ -4,6 +4,8 @@ const fs = require('fs');
 const mongoose = require('mongoose');
 const DB = process.env.DB_URL.replace('<password>', process.env.DB_PASSWORD);
 const Tour = require('../../models/tourModel');
+const User = require('../../models/userModel');
+const Review = require('../../models/reviewModel');
 
 mongoose
   // .connect(process.env.DB_LOCAL_URL, {
@@ -17,10 +19,14 @@ mongoose
   });
 
 const tours = JSON.parse(fs.readFileSync(`${__dirname}/tours.json`, 'utf-8'));
+const users = JSON.parse(fs.readFileSync(`${__dirname}/users.json`, 'utf-8'));
+const reviews = JSON.parse(fs.readFileSync(`${__dirname}/reviews.json`, 'utf-8'));
 
 const importData = async () => {
   try {
+    await User.create(users, { validateBeforeSave: false });
     await Tour.create(tours);
+    await Review.create(reviews);
   } catch (err) {
     console.log(err);
   }
@@ -30,6 +36,8 @@ const importData = async () => {
 const deleteData = async () => {
   try {
     await Tour.deleteMany();
+    await User.deleteMany();
+    await Review.deleteMany();
   } catch (err) {
     console.log(err);
   }
