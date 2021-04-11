cf96a92b5bd770c1a9e7016a55dc13ff568bf43f
sveta <svetlana.shurla4acova@gmail.com>
2021-04-11 14:27:46 +0300

    upload image on frontend

--- public/js/index.js
+++ public/js/index.js
@@ -32,9 +32,12 @@ if (logOutBtn) {
 if (userDataForm) {
   userDataForm.addEventListener('submit', e => {
     e.preventDefault();
-    const name = document.getElementById('name').value;
-    const email = document.getElementById('email').value;
-    updateSettings({ name, email }, 'data');
+    const formData = new FormData();
+    formData.append('name', document.getElementById('name').value);
+    formData.append('email', document.getElementById('email').value);
+    formData.append('photo', document.getElementById('photo').files[0]);
+
+    updateSettings(formData, 'data');
   })
 }
 
